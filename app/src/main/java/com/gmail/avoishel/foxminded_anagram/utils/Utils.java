a115aaf6908b481b3249232dc7b5fd474185f999
Alexandr Voishel <avoishel@gmail.com>
2022-07-03 00:31:34 +0300

    fix: 1: compute word form both side, 2: add logic when filter is empty

--- app/src/main/java/com/gmail/avoishel/foxminded_anagram/utils/Utils.java
+++ app/src/main/java/com/gmail/avoishel/foxminded_anagram/utils/Utils.java
@@ -1,5 +1,9 @@
 package com.gmail.avoishel.foxminded_anagram.utils;
 
+import android.util.Log;
+
+import java.util.regex.Pattern;
+
 public class Utils {
 
     public static String createAnagram(String text, String filter){
@@ -15,15 +19,18 @@ public class Utils {
 
     private static String overTurnAWord(String word, String filter){
 
+        String alphabeticCharacters = "\\w";
         char[] wChar = word.toCharArray();
         int last = word.length();
         int first = 0;
 
         while (first < last){
 
-            if (filter.indexOf(wChar[first])<0) {
+            if (ifCharIsNonAlphabetic(filter, wChar[first])){
                 for (int lastLoop = last-1; lastLoop > first; lastLoop--) {    // --- find symbol from end of the word
-                    if (filter.indexOf(wChar[lastLoop]) < 0 || filter.isEmpty()) {
+
+                    if (ifCharIsNonAlphabetic(filter, wChar[lastLoop])){
+
                         last = lastLoop;
 
                         char buffer = wChar[lastLoop];
@@ -34,9 +41,16 @@ public class Utils {
                     }
                 }
             }
+
             first++;
         }
 
         return String.valueOf(wChar);
+    }
+
+    private static Boolean ifCharIsNonAlphabetic(String filter, char item){
+        return (filter.isEmpty())
+                    ? !(Pattern.matches("[\\W\\d]",String.valueOf(item)))
+                    : (filter.indexOf(item) < 0 );
     }
 }
